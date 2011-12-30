6213278d923097fe8be447ddbe8577edcb44e00b
Igor Minkin <igor.minkin@gmail.com>
2011-12-30 10:22:38 -0500

    changed users controller to disallow create for signed in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
+  before_filter :signed_in_user, :only => [:new, :create]
   
   def index
     @title = "All users"
@@ -75,6 +76,13 @@ class UsersController < ApplicationController
         redirect_to(signin_path)
       else
         redirect_to(root_path) unless current_user.admin?
+      end
+    end
+    
+    def signed_in_user
+      if signed_in?
+        flash[:notice] = "Signed in users can't create a new account."
+        redirect_to(root_path)
       end
     end
 end
